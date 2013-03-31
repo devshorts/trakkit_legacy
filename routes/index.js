fc0e923403d235c333e231249bc6857273a56348
anton <akropp@gmail.com>
2013-03-31 17:57:06 -0400

    clenaup

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,10 @@
  * GET home page.
  */
 
-var twitterRoutes = require("./twitterRoutes");
 var userRoutes = require("./userRoutes");
 var indexRoutes = require("./indexRoutes");
 
 module.exports = function(app){
-    new twitterRoutes.twitterRoutes(app);
     new userRoutes.userRoutes(app);
     new indexRoutes.indexRoutes(app);
 };
